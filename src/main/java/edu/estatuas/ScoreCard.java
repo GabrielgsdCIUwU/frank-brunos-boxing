370f111f3656947b948702e505bffb8fdfe083f9
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-02-27 13:48:04 +0100

    feat(scorecard): Añadir src de scorecard

---
+++ src/main/java/edu/estatuas/ScoreCard.java
@@ -0,0 +1,28 @@
+package edu.estatuas;
+
+public class ScoreCard {
+
+    private static String color;
+    private String redCorner = "";
+    private String blueCorner = "";
+
+    ScoreCard (String color) {
+        this.color = color;
+    }
+
+    public void setRCorner (String boxerName) {
+        redCorner = boxerName;
+    }
+
+    public void setBCorner (String boxerName) {
+        blueCorner = boxerName;
+    }
+
+    String getRCorner() {
+        return redCorner;
+    }
+
+    String getBCorner() {
+        return blueCorner;
+    }
+}
