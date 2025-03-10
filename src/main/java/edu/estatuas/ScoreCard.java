ca6e44e2d82412fc2d186023ce222834c81fc419
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-10 12:22:49 +0100

    feat(scorecard): Añadir judge score card y añadir como mostrar cada ronda

--- src/main/java/edu/estatuas/ScoreCard.java
+++ src/main/java/edu/estatuas/ScoreCard.java
@@ -5,6 +5,7 @@ public class ScoreCard {
     private String color;
     private String redCorner = "";
     private String blueCorner = "";
+    private String[] judgeScoreCard;
 
     ScoreCard (String color) {
         this.color = color;
@@ -24,6 +25,30 @@ public class ScoreCard {
 
     String getBCorner() {
         return blueCorner;
+    }
+
+    public void loadJudgeScoreCard(String[] judgeScoreCard) {
+        this.judgeScoreCard = judgeScoreCard;
+    }
+
+    private String[] getJudgeScoreCard() {
+        return judgeScoreCard;
+    }
+
+    private String getEachScoreRound() {
+        if (getJudgeScoreCard() == null) {return "";}
+        
+        StringBuilder finalMessage = new StringBuilder();
+        for (int i = 0; i < getJudgeScoreCard().length; i++) {
+            String[] scoreBoxerRound = splitScoreCorner(getJudgeScoreCard()[i]);
+            int currentRound = i+1;
+            finalMessage.append(scoreBoxerRound[0] + "\t\t " + currentRound + "\t\t " + scoreBoxerRound[1] + "\n");
+        }
+        return finalMessage.toString();
+    }
+
+    private String[] splitScoreCorner(String scoreRound) {
+        return scoreRound.split(" - ");
     }
 
     @Override
