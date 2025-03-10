f138e5fef729720ae09d44650c5a54e959633d56
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-10 15:11:51 +0100

    feat(roundfactory): Añadir comprobación de knockdownround

--- src/main/java/edu/estatuas/round/RoundFactory.java
+++ src/main/java/edu/estatuas/round/RoundFactory.java
@@ -5,6 +5,8 @@ public class RoundFactory {
     public static Round getRound(String roundScore) {
         if (roundScore.equalsIgnoreCase("10 - 9") || roundScore.equalsIgnoreCase("9 - 10")) {
             return new RegularRound(roundScore);
+        }else if (roundScore.equalsIgnoreCase("10 - 8") || roundScore.equalsIgnoreCase("8 - 10")) {
+            return new KnockdownRound(roundScore);
         }
         return null;
     }
