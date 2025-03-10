3846c4d9e57a9b003fd66c8a52c5aede7e4ef879
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-03-10 15:03:28 +0100

    feat(roundfactory): Añadir la clase roundFactory

---
+++ src/main/java/edu/estatuas/round/RoundFactory.java
@@ -0,0 +1,11 @@
+package edu.estatuas.round;
+
+public class RoundFactory {
+
+    public static Round getRound(String roundScore) {
+        if (roundScore.equalsIgnoreCase("10 - 9") || roundScore.equalsIgnoreCase("9 - 10")) {
+            return new RegularRound(roundScore);
+        }
+        return null;
+    }
+}
